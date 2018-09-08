32bed7d7274401f570ec5b1ed8a85d52d0c424bc
Davide <davidebelli95@gmail.com>
2018-09-08 20:11:10 +0200

    Implemented PyTorch MLP

--- assignment_1/code/mlp_pytorch.py
+++ assignment_1/code/mlp_pytorch.py
@@ -5,16 +5,20 @@ You should fill in code into indicated sections.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 
 class MLP(nn.Module):
-  """
+    """
   This class implements a Multi-layer Perceptron in PyTorch.
   It handles the different layers and parameters of the model.
   Once initialized an MLP object can perform forward.
   """
-
-  def __init__(self, n_inputs, n_hidden, n_classes):
-    """
+    
+    def __init__(self, n_inputs, n_hidden, n_classes):
+        """
     Initializes MLP object. 
     
     Args:
@@ -30,22 +34,32 @@ class MLP(nn.Module):
     TODO:
     Implement initialization of the network.
     """
-
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    self.n_inputs = n_inputs
-    self.n_outputs = n_classes
-    self.n_layers = len(n_hidden)
-    self.size_layers = [n_inputs] + n_hidden
+        
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        super(MLP, self).__init__()
+        
+        
+        self.n_inputs = n_inputs
+        self.n_outputs = n_classes
+        self.n_layers = len(n_hidden)
+        self.size_layers = [n_inputs] + n_hidden
+        
+        self.layers = nn.Sequential()
+        for i in range(self.n_layers):
+            self.layers.add_module("Linear_" + str(i), nn.Linear(self.size_layers[i], self.size_layers[i + 1]))
+            self.layers.add_module("ReLU_" + str(i), nn.ReLU())
+        self.layers.add_module("Linear_" + str(i+1), nn.Linear(self.size_layers[-1], self.n_outputs))
+        self.layers.add_module("Softmax", nn.Softmax(dim=1))
+        
+        
+        ########################
+        # END OF YOUR CODE    #
+        #######################
     
-    
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
-  def forward(self, x):
-    """
+    def forward(self, x):
+        """
     Performs forward pass of the input. Here an input tensor x is transformed through 
     several layer transformations.
     
@@ -57,13 +71,15 @@ class MLP(nn.Module):
     TODO:
     Implement forward pass of the network.
     """
-
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
-    return out
+        
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        for layer in self.layers:
+            x = layer(x)
+        out = x
+        ########################
+        # END OF YOUR CODE    #
+        #######################
+        
+        return out
--- assignment_1/code/train_mlp_pytorch.py
+++ assignment_1/code/train_mlp_pytorch.py
@@ -13,8 +13,14 @@ from mlp_pytorch import MLP
 import cifar10_utils
 
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 # Default constants
 DNN_HIDDEN_UNITS_DEFAULT = '100'
@@ -28,8 +34,9 @@ DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
 
 FLAGS = None
 
-def accuracy(predictions, targets):
-  """
+
+def accuracy(predictions, idx_t):
+    """
   Computes the prediction accuracy, i.e. the average of correct predictions
   of the network.
   
@@ -45,158 +52,178 @@ def accuracy(predictions, targets):
   TODO:
   Implement accuracy computation.
   """
+    
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    _, idx_p = predictions.max(1)
+    correct = [(1 if idx_p[i] == idx_t[i] else 0) for i in range(len(idx_p))]
+    accuracy = sum(correct) / len(correct)
+    ########################
+    # END OF YOUR CODE    #
+    #######################
+    
+    return accuracy
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  idx_p = np.argmax(predictions, axis=1)
-  idx_t = np.argmax(targets, axis=1)
-  correct = [(1 if idx_p[i] == idx_t[i] else 0) for i in range(len(idx_p))]
-  accuracy = sum(correct) / len(correct)
-  ########################
-  # END OF YOUR CODE    #
-  #######################
-
-  return accuracy
 
 def train():
-  """
+    """
   Performs training and evaluation of MLP model. 
 
   TODO:
   Implement training and evaluation of MLP model. Evaluate your model on the whole test set each eval_freq iterations.
   """
-
-  ### DO NOT CHANGE SEEDS!
-  # Set the random seeds for reproducibility
-  np.random.seed(42)
-
-  ## Prepare all functions
-  # Get number of units in each hidden layer specified in the string such as 100,100
-  if FLAGS.dnn_hidden_units:
-    dnn_hidden_units = FLAGS.dnn_hidden_units.split(",")
-    dnn_hidden_units = [int(dnn_hidden_unit_) for dnn_hidden_unit_ in dnn_hidden_units]
-  else:
-    dnn_hidden_units = []
-
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  img_size = 32
-  n_classes = 10
-  input_size = img_size * img_size * 3
-  batch_size = FLAGS.batch_size
-  eval_freq = FLAGS.eval_freq
-  n_iterations = FLAGS.max_steps
-  lr_rate = FLAGS.learning_rate
-
-  def test():
-    x_t = cifar10['test'].images
-    y_t = cifar10['test'].labels
-    x_t = x_t.reshape(-1, input_size)
-    output_t = net.forward(x_t)
-  
-    acc_t = accuracy(output_t, y_t)
-  
-    return acc_t
-
-  def sgd_step():
-    for layer in net.layers:
-      if hasattr(layer, 'params') and hasattr(layer, 'grads'):
-        for key in layer.params.keys():
-          name = type(layer).__name__
-          step = layer.grads[key] * lr_rate
-          layer.params[key] -= step
-    return
-
-  def plot(iteration):
-    idx_test = list(range(0, iteration + 1, eval_freq))
-    idx = list(range(0, iteration + 1))
-  
-    plt.figure(figsize=(10, 4))
-    plt.clf()
-    plt.subplot(1, 2, 1)
-    plt.plot(idx_test, test_accuracies, "k-", linewidth=1, label="test")
-    plt.plot(idx, accuracies, "r-", linewidth=0.5, label="train")
-    plt.xlabel('iteration')
-    plt.ylabel('accuracy')
-    plt.legend()
-  
-    plt.subplot(1, 2, 2)
-    plt.plot(idx, losses, "r-", linewidth=0.5, label="train")
-    plt.xlabel('iteration')
-    plt.ylabel('loss')
-    plt.legend()
-    plt.savefig("plot_pytorch.png", bbox_inches='tight')
-    return
-
-  cifar10 = cifar10_utils.get_cifar10('cifar10/cifar-10-batches-py')
-  net = MLP(input_size, dnn_hidden_units, n_classes)
-  loss = CrossEntropyModule()
-
-  losses = []
-  accuracies = []
-  test_accuracies = []
-
-  for i in range(n_iterations):
-    x, y = cifar10['train'].next_batch(batch_size)
-    x = x.reshape(-1, input_size)
-    output = net.forward(x)
-    preds = np.sum(output, axis=1)
-    loss_value = loss.forward(output, y)
-    loss_grad = loss.backward(output, y)
-    net.backward(loss_grad)
-    sgd_step()
-  
-    losses.append(loss_value)
-    accuracies.append(accuracy(output, y))
-  
-    if i % eval_freq == 0:
-      test_accuracies.append(test())
-      print("[{}/{}] Test Accuracy: {} | Batch Accuracy: {} | Batch Loss: {} |".format(
-        i, n_iterations, test_accuracies[-1], accuracies[-1], loss_value
-      ))
-      plot(i)
     
-      ########################
-  # END OF YOUR CODE    #
-  #######################
+    ### DO NOT CHANGE SEEDS!
+    # Set the random seeds for reproducibility
+    np.random.seed(42)
+    
+    ## Prepare all functions
+    # Get number of units in each hidden layer specified in the string such as 100,100
+    if FLAGS.dnn_hidden_units:
+        dnn_hidden_units = FLAGS.dnn_hidden_units.split(",")
+        dnn_hidden_units = [int(dnn_hidden_unit_) for dnn_hidden_unit_ in dnn_hidden_units]
+    else:
+        dnn_hidden_units = []
+    
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    img_size = 32
+    n_classes = 10
+    input_size = img_size * img_size * 3
+    batch_size = FLAGS.batch_size
+    eval_freq = FLAGS.eval_freq
+    n_iterations = FLAGS.max_steps
+    lr_rate = FLAGS.learning_rate
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = "cpu"
+    print("Device:", device)
+    
+    def test():
+        x_t = cifar10['test'].images
+        y_t = cifar10['test'].labels
+        x_t = torch.from_numpy(x_t.reshape(-1, input_size))
+        y_t = torch.from_numpy(y_t).type(torch.LongTensor)
+        y_t = to_label(y_t)
+        x_t, y_t = x_t.to(device), y_t.to(device)
+        
+        output_t = net(x_t)
+        loss_t = criterion(output_t, y_t)
+        acc_t = accuracy(output_t, y_t)
+        
+        return acc_t, loss_t
+    
+    def plot(iteration):
+        idx_test = list(range(0, iteration + 1, eval_freq))
+        idx = list(range(0, iteration + 1))
+        
+        plt.figure(figsize=(10, 4))
+        plt.clf()
+        plt.cla()
+        plt.subplot(1, 2, 1)
+        plt.plot(idx_test, test_accuracies, "k-", linewidth=1, label="test")
+        plt.plot(idx, accuracies, "r-", linewidth=0.5, alpha=0.5, label="train")
+        plt.xlabel('iteration')
+        plt.ylabel('accuracy')
+        plt.legend()
+        
+        plt.subplot(1, 2, 2)
+        plt.plot(idx_test, test_losses, "k-", linewidth=1, label="test")
+        plt.plot(idx, losses, "r-", linewidth=0.5, alpha=0.5, label="train")
+        plt.xlabel('iteration')
+        plt.ylabel('loss')
+        plt.legend()
+        plt.savefig("plot_pytorch.png", bbox_inches='tight')
+        return
+    
+    def to_label(tensor):
+        _, tensor = tensor.max(1)
+        return tensor
+    
+    cifar10 = cifar10_utils.get_cifar10('cifar10/cifar-10-batches-py')
+    net = MLP(input_size, dnn_hidden_units, n_classes)
+    net.to(device)
+    params = list(net.named_parameters())
+    # net.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=lr_rate, momentum=0.9)
+    
+    losses = []
+    accuracies = []
+    test_accuracies = []
+    test_losses = []
+    
+    for i in range(n_iterations):
+        x, y = cifar10['train'].next_batch(batch_size)
+        x = torch.from_numpy(x.reshape(-1, input_size))
+        y = torch.from_numpy(y).type(torch.LongTensor)
+        y = to_label(y)
+        x, y = x.to(device), y.to(device)
+        
+        optimizer.zero_grad()
+        output = net(x)
+        # output = to_label(output)
+        loss = criterion(output, y)
+        loss.backward()
+        optimizer.step()
+        
+        losses.append(loss)
+        accuracies.append(accuracy(output, y))
+        
+        if i % eval_freq == 0:
+            acc_t, loss_t = test()
+            test_accuracies.append(acc_t)
+            test_losses.append(loss_t)
+            print("[{}/{}] Test Accuracy: {} | Batch Accuracy: {} | Batch Loss: {} |".format(
+                i, n_iterations, test_accuracies[-1], accuracies[-1], loss
+            ))
+            plot(i)
+            
+            
+    ########################
+    # END OF YOUR CODE    #
+    #######################
+
 
 def print_flags():
-  """
+    """
   Prints all entries in FLAGS variable.
   """
-  for key, value in vars(FLAGS).items():
-    print(key + ' : ' + str(value))
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
+
 
 def main():
-  """
+    """
   Main function
   """
-  # Print all Flags to confirm parameter settings
-  print_flags()
+    # Print all Flags to confirm parameter settings
+    print_flags()
+    
+    if not os.path.exists(FLAGS.data_dir):
+        os.makedirs(FLAGS.data_dir)
+    
+    # Run the training operation
+    train()
 
-  if not os.path.exists(FLAGS.data_dir):
-    os.makedirs(FLAGS.data_dir)
-
-  # Run the training operation
-  train()
 
 if __name__ == '__main__':
-  # Command line arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--dnn_hidden_units', type = str, default = DNN_HIDDEN_UNITS_DEFAULT,
-                      help='Comma separated list of number of units in each hidden layer')
-  parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
-                      help='Learning rate')
-  parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
-                      help='Number of steps to run trainer.')
-  parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
-                      help='Batch size to run trainer.')
-  parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dnn_hidden_units', type=str, default=DNN_HIDDEN_UNITS_DEFAULT,
+                        help='Comma separated list of number of units in each hidden layer')
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE_DEFAULT,
+                        help='Learning rate')
+    parser.add_argument('--max_steps', type=int, default=MAX_STEPS_DEFAULT,
+                        help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE_DEFAULT,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
                         help='Frequency of evaluation on the test set')
-  parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-
-  main()
+    parser.add_argument('--data_dir', type=str, default=DATA_DIR_DEFAULT,
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
+    
+    main()
