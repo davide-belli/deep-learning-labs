7f4d261b50c0c784c79caa6cf8eb038730c678cd
Davide <davidebelli95@gmail.com>
2018-09-11 23:05:28 +0200

    Convnet implementation

--- assignment_1/code/convnet_pytorch.py
+++ assignment_1/code/convnet_pytorch.py
@@ -5,55 +5,108 @@ You should fill in code into indicated sections.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import torch
+import torch.nn as nn
+
 
 class ConvNet(nn.Module):
-  """
-  This class implements a Convolutional Neural Network in PyTorch.
-  It handles the different layers and parameters of the model.
-  Once initialized an ConvNet object can perform forward.
-  """
-
-  def __init__(self, n_channels, n_classes):
     """
-    Initializes ConvNet object. 
-    
-    Args:
-      n_channels: number of input channels
-      n_classes: number of classes of the classification problem
-                 
-    
-    TODO:
-    Implement initialization of the network.
+    This class implements a Convolutional Neural Network in PyTorch.
+    It handles the different layers and parameters of the model.
+    Once initialized an ConvNet object can perform forward.
     """
-
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
-  def forward(self, x):
-    """
-    Performs forward pass of the input. Here an input tensor x is transformed through 
-    several layer transformations.
     
-    Args:
-      x: input to the network
-    Returns:
-      out: outputs of the network
+    def __init__(self, n_channels, n_classes):
+        """
+        Initializes ConvNet object.
+        
+        Args:
+          n_channels: number of input channels
+          n_classes: number of classes of the classification problem
+          
+        
+        TODO:
+        Implement initialization of the network.
+        """
+        
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+        batchnorm = nn.BatchNorm1d()
+        relu = nn.ReLU()
+        
+        self.layers = nn.Sequential()
+        
+        self.layers.add_module("conv_1", nn.Conv2d(n_channels, 64, 3))
+        self.layers.add_module("batchnorm_1", batchnorm)
+        self.layers.add_module("relu_1", relu)
+
+        self.layers.add_module("maxpool_1", maxpool)
+        
+        self.layers.add_module("conv_2", nn.Conv2d(64, 128, 3))
+        self.layers.add_module("batchnorm_2", batchnorm)
+        self.layers.add_module("relu_2", relu)
+
+        self.layers.add_module("maxpool_2", maxpool)
+        
+        self.layers.add_module("conv_3_a", nn.Conv2d(128, 256, 3))
+        self.layers.add_module("batchnorm_3_a", batchnorm)
+        self.layers.add_module("relu_3_a", relu)
+        self.layers.add_module("conv_3_b", nn.Conv2d(256, 256, 3))
+        self.layers.add_module("batchnorm_3_b", batchnorm)
+        self.layers.add_module("relu_3_b", relu)
+
+        self.layers.add_module("maxpool_3", maxpool)
+        
+        self.layers.add_module("conv_4_a", nn.Conv2d(256, 512, 3))
+        self.layers.add_module("batchnorm_4_a", batchnorm)
+        self.layers.add_module("relu_4_a", relu)
+        self.layers.add_module("conv_4_b", nn.Conv2d(512, 512, 3))
+        self.layers.add_module("batchnorm_4_b", batchnorm)
+        self.layers.add_module("relu_4_b", relu)
+
+        self.layers.add_module("maxpool_4", maxpool)
+        
+        self.layers.add_module("conv_5_a", nn.Conv2d(512, 512, 3))
+        self.layers.add_module("batchnorm_5_a", batchnorm)
+        self.layers.add_module("relu_5_a", relu)
+        self.layers.add_module("conv_5_b", nn.Conv2d(512, 512, 3))
+        self.layers.add_module("batchnorm_5_b", batchnorm)
+        self.layers.add_module("relu_5_b", relu)
+        
+        self.layers.add_module("maxpool_5", maxpool)
+        
+        self.layers.add_module("avgpool", nn.AvgPool2d())
+        
+        self.layers.add_module("linear", nn.Linear(512, n_classes))
+        self.layers.add_module("softmax", nn.Softmax(dim=1))
+
+        self.sequential = nn.Sequential(*self.layers)
+        ########################
+        # END OF YOUR CODE    #
+        #######################
     
-    TODO:
-    Implement forward pass of the network.
-    """
-
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
-
-    return out
+    def forward(self, x):
+        """
+        Performs forward pass of the input. Here an input tensor x is transformed through
+        several layer transformations.
+        
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+        
+        TODO:
+        Implement forward pass of the network.
+        """
+        
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        out = self.sequential(x)
+        ########################
+        # END OF YOUR CODE    #
+        #######################
+        
+        return out
--- assignment_1/code/train_convnet_pytorch.py
+++ assignment_1/code/train_convnet_pytorch.py
@@ -11,6 +11,14 @@ import numpy as np
 import os
 from convnet_pytorch import ConvNet
 import cifar10_utils
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 # Default constants
 LEARNING_RATE_DEFAULT = 1e-4
@@ -24,87 +32,199 @@ DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
 
 FLAGS = None
 
+
 def accuracy(predictions, targets):
-  """
-  Computes the prediction accuracy, i.e. the average of correct predictions
-  of the network.
-  
-  Args:
-    predictions: 2D float array of size [batch_size, n_classes]
-    labels: 2D int array of size [batch_size, n_classes]
-            with one-hot encoding. Ground truth labels for
-            each sample in the batch
-  Returns:
-    accuracy: scalar float, the accuracy of predictions,
-              i.e. the average correct predictions over the whole batch
-  
-  TODO:
-  Implement accuracy computation.
-  """
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
+    
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes]
+      labels: 2D int array of size [batch_size, n_classes]
+              with one-hot encoding. Ground truth labels for
+              each sample in the batch
+    Returns:
+      accuracy: scalar float, the accuracy of predictions,
+                i.e. the average correct predictions over the whole batch
+    
+    TODO:
+    Implement accuracy computation.
+    """
+    
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    _, idx_p = predictions.max(1)
+    _, idx_t = targets.max(1)
+    correct = [(1 if idx_p[i] == idx_t[i] else 0) for i in range(len(idx_p))]
+    accuracy = sum(correct) / len(correct)
+    ########################
+    # END OF YOUR CODE    #
+    #######################
+    
+    return accuracy
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
-
-  return accuracy
 
 def train():
-  """
-  Performs training and evaluation of ConvNet model. 
+    """
+    Performs training and evaluation of ConvNet model.
+  
+    TODO:
+    Implement training and evaluation of ConvNet model. Evaluate your model on the whole test set each eval_freq iterations.
+    """
+    
+    ### DO NOT CHANGE SEEDS!
+    # Set the random seeds for reproducibility
+    np.random.seed(42)
+    
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    img_size = 32
+    input_size = img_size * img_size * 3
+    batch_size = FLAGS.batch_size
+    eval_freq = FLAGS.eval_freq
+    n_iterations = FLAGS.max_steps
+    lr_rate = FLAGS.learning_rate
 
-  TODO:
-  Implement training and evaluation of ConvNet model. Evaluate your model on the whole test set each eval_freq iterations.
-  """
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Device:", device)
 
-  ### DO NOT CHANGE SEEDS!
-  # Set the random seeds for reproducibility
-  np.random.seed(42)
+    def test():
+        net.eval()
+    
+        output_t = net(x_t)
+        loss_t = criterion(output_t, y_t)
+        acc_t = accuracy(output_t, y_t)
+    
+        return acc_t, loss_t
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    def plot(iteration):
+        idx_test = list(range(0, iteration + 1, eval_freq))
+        idx = list(range(0, iteration + 1))
+    
+        plt.clf()
+        plt.cla()
+        plt.subplot(1, 2, 1)
+        plt.plot(idx_test, test_accuracies, "k-", linewidth=1, label="test")
+        plt.plot(idx, accuracies, "r-", linewidth=0.5, alpha=0.5, label="train")
+        plt.xlabel('iteration')
+        plt.ylabel('accuracy')
+        plt.legend()
+    
+        plt.subplot(1, 2, 2)
+        plt.plot(idx_test, test_losses, "k-", linewidth=1, label="test")
+        plt.plot(idx, losses, "r-", linewidth=0.5, alpha=0.5, label="train")
+        plt.xlabel('iteration')
+        plt.ylabel('loss')
+        plt.legend()
+        plt.savefig("plot_convnet.png", bbox_inches='tight')
+        return
+
+    def to_label(tensor):
+        _, tensor = tensor.max(1)
+        return tensor
+
+    cifar10 = cifar10_utils.get_cifar10('cifar10/cifar-10-batches-py')
+    net = ConvNet(3, 10)
+    net.to(device)
+    params = list(net.named_parameters())
+    net.to(device)
+    criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.SGD(net.parameters(), lr=lr_rate, momentum=0.8, nesterov=False)
+    optimizer = optim.Adam(net.parameters(), lr=lr_rate)
+
+    losses = []
+    accuracies = []
+    test_accuracies = []
+    test_losses = []
+    alpha = 0.0001
+
+    x_t = cifar10['test'].images
+    y_t = cifar10['test'].labels
+    x_t = torch.from_numpy(x_t.reshape(-1, input_size))
+    y_t = torch.from_numpy(y_t).type(torch.LongTensor)
+    y_t = to_label(y_t)
+    x_t, y_t = x_t.to(device), y_t.to(device)
+
+    plt.figure(figsize=(10, 4))
+
+    for i in range(n_iterations):
+    
+        x, y = cifar10['train'].next_batch(batch_size)
+        x = torch.from_numpy(x)
+        y_onehot = torch.from_numpy(y).type(torch.LongTensor)
+        y = to_label(y_onehot)
+        x, y = x.to(device), y.to(device)
+    
+        optimizer.zero_grad()
+        output = net(x)
+        train_loss = criterion(output, y)
+    
+        reg_loss = 0
+        for param in net.parameters():
+            reg_loss += param.norm(2)
+    
+        loss = train_loss + alpha * reg_loss
+        loss.backward()
+        optimizer.step()
+    
+        losses.append(loss.item())
+        accuracies.append(accuracy(output, y))
+    
+        del x, y
+    
+        if i % eval_freq == 0:
+            acc_t, loss_t = test()
+            test_accuracies.append(acc_t)
+            test_losses.append(loss_t)
+            print("[{}/{}] Test Accuracy: {} | Batch Accuracy: {} | Batch Loss: {} | Train/Reg: {}/{}".format(
+                i, n_iterations, test_accuracies[-1], accuracies[-1], loss, train_loss, reg_loss * alpha
+            ))
+            plot(i)
+        
+            net.train()
+
+    ########################
+    # END OF YOUR CODE    #
+    #######################
+
 
 def print_flags():
-  """
-  Prints all entries in FLAGS variable.
-  """
-  for key, value in vars(FLAGS).items():
-    print(key + ' : ' + str(value))
+    """
+    Prints all entries in FLAGS variable.
+    """
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
+
 
 def main():
-  """
-  Main function
-  """
-  # Print all Flags to confirm parameter settings
-  print_flags()
+    """
+    Main function
+    """
+    # Print all Flags to confirm parameter settings
+    print_flags()
+    
+    if not os.path.exists(FLAGS.data_dir):
+        os.makedirs(FLAGS.data_dir)
+    
+    # Run the training operation
+    train()
 
-  if not os.path.exists(FLAGS.data_dir):
-    os.makedirs(FLAGS.data_dir)
-
-  # Run the training operation
-  train()
 
 if __name__ == '__main__':
-  # Command line arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
-                      help='Learning rate')
-  parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
-                      help='Number of steps to run trainer.')
-  parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
-                      help='Batch size to run trainer.')
-  parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE_DEFAULT,
+                        help='Learning rate')
+    parser.add_argument('--max_steps', type=int, default=MAX_STEPS_DEFAULT,
+                        help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE_DEFAULT,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--eval_freq', type=int, default=EVAL_FREQ_DEFAULT,
                         help='Frequency of evaluation on the test set')
-  parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-
-  main()
+    parser.add_argument('--data_dir', type=str, default=DATA_DIR_DEFAULT,
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
+    
+    main()
