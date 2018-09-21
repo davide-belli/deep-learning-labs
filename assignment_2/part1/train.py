9cb13edced9a44bbf8551486506507b0b3ace9a2
Davide <davidebelli95@gmail.com>
2018-09-21 18:47:36 +0200

    Fixed and completed vanilla_rnn

--- assignment_2/part1/train.py
+++ assignment_2/part1/train.py
@@ -33,86 +33,139 @@ from part1.lstm import LSTM
 # You may want to look into tensorboardX for logging
 # from tensorboardX import SummaryWriter
 
+from tensorboardX import SummaryWriter
+
+
 ################################################################################
 
 def train(config):
-
     assert config.model_type in ('RNN', 'LSTM')
-
+    
+    exp_name = 'runs/{}_batch{}_dim{}_len{}'.format(datetime.now().strftime("%Y-%m-%d %H:%M"), config.batch_size,
+                                             config.input_dim, config.input_length)
+    print(exp_name)
+    print(config)
+    
+    writer = SummaryWriter(exp_name)
+    
+    def to_label(tensor):
+        _, tensor = tensor.max(-1)
+        return tensor
+    
+    def to_one_hot(values):
+        values = values.view(values.shape[0], -1)
+        tensor = torch.zeros(values.shape[0], values.shape[1], config.num_classes)
+        for i in range(values.shape[0]):
+            for j in range(values.shape[1]):
+                tensor.data[i, j, values[i, j]] = 1
+        return tensor
+    
+    def get_accuracy(predictions, targets):
+        idx_p = to_label(predictions)
+        idx_t = targets
+        correct = [(1 if idx_p[i] == idx_t[i] else 0) for i in range(len(idx_p))]
+        accuracy = sum(correct) / len(correct)
+        return accuracy
+    
     # Initialize the device which to run the model on
     device = torch.device(config.device)
-
+    print(device)
+    
     # Initialize the model that we are going to use
-    model = None  # fixme
-
+    if config.model_type == 'RNN':
+        model = VanillaRNN(config.input_length, config.input_dim, config.num_hidden,
+                           config.num_classes, config.batch_size, device)
+    else:
+        model = None
+    
+    # print(list(model.named_parameters()))
+    
     # Initialize the dataset and data loader (note the +1)
-    dataset = PalindromeDataset(config.input_length+1)
+    dataset = PalindromeDataset(config.input_length + 1)
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
-
+    
     # Setup the loss and optimizer
-    criterion = None  # fixme
-    optimizer = None  # fixme
-
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.RMSprop(model.parameters(), config.learning_rate)
+    
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
-
+        
         # Only for time measurement of step through network
         t1 = time.time()
-
+        
         # Add more code here ...
-
+        model.train()
+        optimizer.zero_grad()
+        
+        # Add more code here ...
+        batch_targets = batch_targets.to(device)
+        
+        if config.input_dim == 1:
+            batch_inputs = batch_inputs.unsqueeze(-1).to(device)
+            batch_outputs = model(batch_inputs)
+        elif config.input_dim == 10:
+            batch_inputs_long = batch_inputs.type(torch.LongTensor)
+            batch_inputs_onehot = to_one_hot(batch_inputs_long).type(torch.FloatTensor).to(device)
+            batch_outputs = model(batch_inputs_onehot)
+        
+        loss = criterion(batch_outputs, batch_targets)
+        accuracy = get_accuracy(batch_outputs, batch_targets)
+        
+        loss.backward()
+        
         ############################################################################
         # QUESTION: what happens here and why?
         ############################################################################
         torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=config.max_norm)
         ############################################################################
-
-        # Add more code here ...
-
-        loss = np.inf   # fixme
-        accuracy = 0.0  # fixme
-
+        # The gradients are clipped to avoid exploding gradient problem
+        
+        optimizer.step()
+        
+        # with torch.no_grad():
+        
         # Just for time measurement
         t2 = time.time()
-        examples_per_second = config.batch_size/float(t2-t1)
-
-        if step % 10 == 0:
-
+        examples_per_second = config.batch_size / float(t2 - t1)
+        
+        if step % 100 == 0:
             print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
                   "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, examples_per_second,
-                    accuracy, loss
+                datetime.now().strftime("%Y-%m-%d %H:%M"), step,
+                config.train_steps, config.batch_size, examples_per_second,
+                accuracy, loss
             ))
-
+            writer.add_scalar('Accuracy', accuracy, step)
+            writer.add_scalar('Loss', loss.item(), step)
+        
         if step == config.train_steps:
             # If you receive a PyTorch data-loader error, check this bug report:
             # https://github.com/pytorch/pytorch/pull/9655
             break
-
+    
     print('Done training.')
 
 
- ################################################################################
- ################################################################################
+################################################################################
+################################################################################
 
 if __name__ == "__main__":
-
     # Parse training configuration
     parser = argparse.ArgumentParser()
-
+    
     # Model params
     parser.add_argument('--model_type', type=str, default="RNN", help="Model type, should be 'RNN' or 'LSTM'")
-    parser.add_argument('--input_length', type=int, default=10, help='Length of an input sequence')
-    parser.add_argument('--input_dim', type=int, default=1, help='Dimensionality of input sequence')
+    parser.add_argument('--input_length', type=int, default=6, help='Length of an input sequence')
+    parser.add_argument('--input_dim', type=int, default=10, help='Dimensionality of input sequence')
     parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
-    parser.add_argument('--num_hidden', type=int, default=128, help='Number of hidden units in the model')
+    parser.add_argument('--num_hidden', type=int, default=64, help='Number of hidden units in the model')
     parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--train_steps', type=int, default=10000, help='Number of training steps')
     parser.add_argument('--max_norm', type=float, default=10.0)
     parser.add_argument('--device', type=str, default="cuda:0", help="Training device 'cpu' or 'cuda:0'")
-
+    
     config = parser.parse_args()
-
+    
     # Train the model
     train(config)
--- assignment_2/part1/vanilla_rnn.py
+++ assignment_2/part1/vanilla_rnn.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 
+
 ################################################################################
 
 class VanillaRNN(nn.Module):
@@ -28,7 +29,47 @@ class VanillaRNN(nn.Module):
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(VanillaRNN, self).__init__()
         # Initialization here ...
+    
+        self.seq_len = seq_length
+        self.input_dim = input_dim
+        self.num_hidden = num_hidden
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+        self.device = device
+        
+        self.b_h = torch.nn.Parameter(torch.Tensor(self.num_hidden, 1).zero_().to(self.device))
+        self.W_hh = torch.nn.Parameter(torch.nn.init.orthogonal_(torch.Tensor(self.num_hidden, self.num_hidden).normal_(mean=0, std=0.0001)).to(self.device))
+        self.W_hx = torch.nn.Parameter(torch.Tensor(self.num_hidden, self.input_dim).normal_(mean=0, std=0.0001).to(self.device))
+        
+        self.b_p = torch.nn.Parameter(torch.Tensor(self.num_classes, 1).zero_().to(self.device))
+        self.W_ph = torch.nn.Parameter(torch.Tensor(self.num_classes, self.num_hidden).normal_(mean=0, std=0.0001).to(self.device))
+        
+        self.tanh = torch.nn.Tanh()
+        # self.softmax = torch.nn.Softmax(dim=0)
 
     def forward(self, x):
         # Implementation here ...
+        
+        p = None
+            
+        h = torch.zeros(self.num_hidden, self.batch_size).to(self.device)
+
+        for i in range(self.seq_len):
+            a1 = self.W_hx
+            a2 = x[:, i].reshape(self.input_dim, -1)  # Deals with input of any dimensionality
+            a = a1 @ a2
+            b1 = self.W_hh
+            b2 = h
+            b = b1 @ b2
+            c = self.b_h
+            h_hat = a + b + c
+            h = self.tanh(h_hat)
+            
+        r = self.W_ph @ h
+        s = self.b_p
+        # p = self.softmax(r + s)
+        p = r + s
+        
+        return p.reshape(self.batch_size, -1)
+        
         pass
