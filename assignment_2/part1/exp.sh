7e9e91f2d5bc614ad9c688f59684ae3ef8a33d5b
Davide <davidebelli95@gmail.com>
2018-09-27 13:26:59 +0200

    Final commit part1, exp with dim=10

--- assignment_2/part1/exp.sh
+++ assignment_2/part1/exp.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-for k in 128 512
+#PBS -qgpu
+#PBS -lwalltime=10:00:00
+#PBS -S /bin/bash
+#PBS -lnodes=1:ppn=12
+#PBS -lmem=250G
+
+
+cd /home/lgpu0009/assignment_2/part1
+rm ./*.sh.*
+
+source activate base
+export PYTHONPATH=home/lgpu0009/assignment_2/part1
+
+for k in 128
 do
-	for i in "RNN" "LSTM" 
-	do
-		for j in 30 20 15 10 5
-		do
-	    	python train.py --input_length $j --model_type $i --batch_size $k
-		done
-	done
+        for i in "RNN" "LSTM" 
+        do
+                for j in 50 40 30 20 10 5 6 7 8 9 11 12 13 14 15
+                do
+                python train.py --input_length $j --model_type $i --batch_size $k
+                done
+        done
 done
